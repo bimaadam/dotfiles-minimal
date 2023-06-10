4b7a7cd05e0530b686be0748b4835ef8441abd68
bimaadam <bimaadam.dev@gmail.com>
2023-06-10 10:49:03 +0700

    feat: add minimal 'Screenshot saved' notification UX to screenshot script

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -4,11 +4,20 @@ TARGET_DIR="$HOME/Pictures"
 mkdir -p "$TARGET_DIR"
 FILENAME="$TARGET_DIR/$(date +%Y-%m-%d_%H-%M-%S).png"
 
+notify_user() {
+    if command -v notify-send >/dev/null 2>&1 && (pgrep -x dunst >/dev/null 2>&1 || pgrep -x mako >/dev/null 2>&1 || pgrep -x swaync >/dev/null 2>&1); then
+        notify-send -t 1500 -u low "Screenshot saved"
+    elif command -v hyprctl >/dev/null 2>&1; then
+        hyprctl notify 0 1500 "rgb(ffffff)" "Screenshot saved"
+    fi
+}
+
 case "${1:-full}" in
     full)
         grim "$FILENAME"
         if [[ -s "$FILENAME" ]]; then
             wl-copy --type image/png < "$FILENAME"
+            notify_user
         fi
         ;;
     area)
@@ -17,6 +26,7 @@ case "${1:-full}" in
             grim -g "$GEOM" "$FILENAME"
             if [[ -s "$FILENAME" ]]; then
                 wl-copy --type image/png < "$FILENAME"
+                notify_user
             fi
         fi
         ;;
