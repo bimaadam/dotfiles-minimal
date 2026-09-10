a104138af0bf44d42eea08ec71cd25adaa892fa8
bimaadam <bimaadam.dev@gmail.com>
2026-09-10 13:50:21 +0700

    feat(nvim): configure language packs, lazygit, nodejs, and monochrome theme

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,12 +1,22 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- Languages & Packs
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.hyprlang" },
+
+  -- Monochrome Theme
+  { import = "astrocommunity.colorscheme.lackluster-nvim" },
 }
